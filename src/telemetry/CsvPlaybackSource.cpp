a1a87fbc9e7739c4dca52b42595e3aad806dea31
Ryan Vu <ryanvu@frc4322.com>
2026-06-07 03:32:23 -0700

    implement CsvPlayback as telemetry source

---
+++ src/telemetry/CsvPlaybackSource.cpp
@@ -0,0 +1,83 @@
+#include "CsvPlaybackSource.h"
+
+#include <QDateTime>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+#include <QDateTime>
+#include <QVector>
+#include <algorithm>
+
+CsvPlaybackSource::CsvPlaybackSource(QString csvPath, QObject *parent)
+    : TelemetrySource(parent)
+{
+    connect(&m_timer, &QTimer::timeout, this, &CsvPlaybackSource::tick);
+    m_csvPath = csvPath;
+}
+
+void CsvPlaybackSource::loadCsv()
+{
+    QFile csvFile(m_csvPath);
+    if (!csvFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qWarning() << "Failed to open CSV file" << m_csvPath;
+        return;
+    }
+
+    QTextStream csvStream(&csvFile);
+    
+    csvStream.readLine(); // skip header line
+    while (!csvStream.atEnd()) {
+        const QString line = csvStream.readLine();
+        const QStringList fields = line.split(',');
+        if (fields.size() != 8) {
+            continue;
+        }
+        TimedSample timedSample;
+        timedSample.playbackTimeMillis = fields[0].toLongLong();
+        timedSample.telemetry.motorId = fields[1].toInt();
+        timedSample.telemetry.rpm = fields[2].toDouble();
+        timedSample.telemetry.voltage = fields[3].toDouble();
+        timedSample.telemetry.current = fields[4].toDouble();
+        timedSample.telemetry.temperatureCelsius = fields[5].toDouble();
+        timedSample.telemetry.pwm = fields[6].toDouble();
+        timedSample.telemetry.status = fields[7];
+        m_samples.append(timedSample);
+    }
+
+    std::sort(m_samples.begin(), m_samples.end(),
+                [](const TimedSample &a, const TimedSample &b) {
+                    return a.playbackTimeMillis < b.playbackTimeMillis;
+                });
+
+    csvFile.close();
+    qDebug() << "Loaded" << m_samples.size() << "samples from" << m_csvPath;
+}
+
+void CsvPlaybackSource::start()
+{
+    m_nextSampleIndex = 0;
+    m_startedAtMillis = QDateTime::currentMSecsSinceEpoch();
+    m_samples.clear();
+    loadCsv();
+    m_timer.start();
+}
+
+void CsvPlaybackSource::stop()
+{
+    m_timer.stop();
+}
+
+void CsvPlaybackSource::tick()
+{
+    const qint64 elapsedMillis = QDateTime::currentMSecsSinceEpoch() - m_startedAtMillis;
+
+    while (m_nextSampleIndex < m_samples.size() &&
+           m_samples[m_nextSampleIndex].playbackTimeMillis <= elapsedMillis) {
+        emit telemetryReceived(m_samples[m_nextSampleIndex].telemetry);
+        ++m_nextSampleIndex;
+    }
+
+    if (m_nextSampleIndex >= m_samples.size()) {
+        stop();
+    }
+}
---
+++ src/telemetry/CsvPlaybackSource.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "TelemetrySource.h"
+
+#include <QTimer>
+#include <QRandomGenerator>
+
+class CsvPlaybackSource : public TelemetrySource
+{
+    Q_OBJECT
+
+public:
+    explicit CsvPlaybackSource(QString csvPath, QObject *parent = nullptr);
+
+    void start() override;
+    void stop() override;
+
+private:
+    struct TimedSample {
+        qint64 playbackTimeMillis = 0;
+        MotorTelemetry telemetry;
+    };
+
+    void loadCsv();
+    void tick();
+
+    QVector<TimedSample> m_samples;
+    qsizetype m_nextSampleIndex = 0;
+    qint64 m_startedAtMillis = 0;
+    QTimer m_timer;
+    QString m_csvPath;
+};
